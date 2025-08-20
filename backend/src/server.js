10d2049307a30f7cb6aae1591548998924d5ff1e
miraj <mirajhowlader9999@gmail.ocm>
2025-08-21 01:30:16 +0600

    added some tools

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,35 @@
 import express from 'express';
 import { ENV } from './config/env.js';
 import connectDB from './config/db.js';
+import { serve } from "inngest/express";
+import {clerkMiddleware} from '@clerk/express'
+import { inngest,functions } from './config/inngest.js';
 
 
 const app = express();
+app.use(clerkMiddleware())
+app.use(express.json());
+
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 
 
 
-app.listen(ENV.PORT, () => {
-  connectDB();
-  console.log('Server is running on port:' +ENV.PORT);
-})
+const startServer = async () => {
+  try {
+    await connectDB();
+    if(ENV.NODE_ENV !== "production"){
+      app.listen(ENV.PORT, () => {
+        console.log(`Server running on port ${ENV.PORT}`);
+      })
+    }
+  } catch (error) {
+    console.log("error", error)
+    process.exit(1)
+  }
+}
+
+startServer();
+
+export default app;
